e2b8ed7bcc4775408fce823fa8fbf6184de37389
daindragon2 <g.per45@gmail.com>
2017-04-19 00:09:39 +0200

    Update remove_git.sh

--- remove_git.sh
+++ remove_git.sh
@@ -20,5 +20,4 @@ fi
 for i in $(cat $1  | sed 1,2d  | awk '{print $4}'); do git filter-branch --tag-name-filter cat --index-filter  "git rm -r --cached --ignore-unmatch $i"   --prune-empty -f -- --all;  done; 
 
 # Then reflog and clean cache 
-rm -rf .git/refs/original/ ; git reflog expire --expire=now --all ;  git gc --prune=now; git gc --aggressive --prune=now
-~                                                                                                                              
+rm -rf .git/refs/original/ ; git reflog expire --expire=now --all ;  git gc --prune=now; git gc --aggressive --prune=now 
