c482d72d694e6d7d68600afcead4d0c72acdfcc2
daindragon2 <g.per45@gmail.com>
2017-04-18 21:19:53 +0200

    Update check_git.sh

--- check_git.sh
+++ check_git.sh
@@ -10,8 +10,28 @@
 # set the internal field spereator to line break, so that we can iterate easily over the verify-pack output
 IFS=$'\n';
 
+
+
+
+#check number
+if [ $# -eq 1 ]; then
+
+
+          if [ "$1" -eq "$1" ] 2>/dev/null; then 
+                numObj=$1
+        else 
+        echo "no number object given, set to default (10) "
+        numObj=10
+         fi  
+else 
+echo "no number object given, set to default (10) "
+numObj=10
+fi
+
+
 # list all objects including their size, sort by size, take top 10
-objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head`
+
+objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head -n$numObj `
 
 echo "All sizes are in kB. The pack column is the size of the object, compressed, inside the pack file."
 
