d5987904c2806e7971eaff2880b2bb800bde5d08
daindragon2 <g.per45@gmail.com>
2017-04-18 21:18:57 +0200

    Create remove_git.sh

---
+++ remove_git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author:       giper
+# Email:        g.per45@gmail.com
+# Date:          
+# Description:
+# 
+#
+#
+
+usage() {
+echo $1
+echo "Usage to_remove.sh [filetoremove]"
+exit 1
+}
+
+if [ $# -ne 1 ]; then
+   usage "pls gives the file to remove"
+fi
+
+for i in $(cat $1  | sed 1,2d  | awk '{print $4}'); do git filter-branch --tag-name-filter cat --index-filter  "git rm -r --cached --ignore-unmatch $i"   --prune-empty -f -- --all;  done; 
+
+# Then reflog and clean cache 
+rm -rf .git/refs/original/ ; git reflog expire --expire=now --all ;  git gc --prune=now; git gc --aggressive --prune=now
+~                                                                                                                              
